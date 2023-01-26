45d0826b403a4c778068aa2eabeda1fbc6acd85d
Stepan Ryabukhin <ryabuhin.stepan@yandex.ru>
2023-01-26 21:40:13 +0500

    fix example

--- nodejs18-features/fetchApi/stream-example.js
+++ nodejs18-features/fetchApi/stream-example.js
@@ -4,10 +4,15 @@ import {Readable} from 'node:stream';
 await (async () => {
     const beforeUsedMemory = process.memoryUsage().heapUsed / 1024 / 1024;
     const res = await fetch("https://github.com/json-iterator/test-data/raw/master/large-file.json");
+
+    const writeStream = fs.createWriteStream("response-second.json");
+    writeStream.on("finish", () => {
+        const afterUsedMemory = process.memoryUsage().heapUsed / 1024 / 1024;
+        console.log("before used memory: ", beforeUsedMemory, "Mb", "|", "after used memory: ", afterUsedMemory, "Mb")
+    })
+
     if (res.ok) {
-        Readable.fromWeb(res.body).pipe(fs.createWriteStream(("response-second.json")))
+        Readable.fromWeb(res.body).pipe(writeStream);
     }
-    const afterUsedMemory = process.memoryUsage().heapUsed / 1024 / 1024;
-    console.log("before used memory: ", beforeUsedMemory, "Mb", "|", "after used memory: ", afterUsedMemory, "Mb")
 })();
 
